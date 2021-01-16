5f1de6d4d442fb8ab2aa1fe424d5952d5d0c587b
Gavin Rehkemper <gavin@gavinr.com>
2021-01-16 14:20:03 -0600

    USL script update

--- scripts/get-usl.js
+++ scripts/get-usl.js
@@ -1,5 +1,5 @@
 // run this in the console on page 
-// https://en.wikipedia.org/wiki/USL_Championship
+// https://en.wikipedia.org/wiki/USL_Championship#Current_clubs
 
 
 var getLocation = function (title) {
@@ -34,18 +34,43 @@ var getLocation = function (title) {
   });
 };
 
+var getWebsite = function (title) {
+  // await fetch(url);
+  var url = new URL('https://en.wikipedia.org/api/rest_v1/page/html/' + title);
+  
+  return new Promise(async (resolve, reject) => {
+
+    const results = await fetch(url);
+    const textResults = await results.text();
+
+    const parts = textResults.split('<h2 id="External_links">External links</h2>');
+    if(parts.length > 1) {
+      const parts2 = parts[1].split('<a rel="mw:ExtLink" href="');
+      const parts3 = parts2[1].split('"');
+      var u = new URL(parts3[0]);
+      resolve(u.href);
+    } else {
+      // else:
+      console.error('error in parsing', title);
+      resolve(false);
+    }
+
+  });
+};
+
 
 const results = $('.wikitable').eq(1).find('tr').get().map(async function(tr) {
-  console.log('tr:', tr);
   if($(tr).find('td').eq(0).find('a').text()) {
 
     var stadiumLink = $(tr).find('td').eq(2).find('a').eq(0).attr('href');
     var location = await getLocation(decodeURIComponent(stadiumLink.replace('/wiki/', '')))
 
+    var teamLink = $(tr).find('td').eq(0).find('a').eq(0).attr('href');
+    var website = await getWebsite(decodeURIComponent(teamLink.replace('/wiki/', '')));
+
     if(location) {
       
       // length: $(tr).find('td').length
-      console.log('FIND', $(tr).find('td').eq(1).find('a').eq(0).text().split(','));
       let retArr =  [
         $(tr).find('td').eq(0).find('a').eq(0).text(), // team name
         $(tr).find('td').eq(1).find('a').eq(0).text().split(',')[0].trim(), // city
@@ -69,6 +94,9 @@ const results = $('.wikitable').eq(1).find('tr').get().map(async function(tr) {
         retArr.push($(tr).find('td').eq(5).find('a').eq(1).text().trim()); // head_coach
         retArr.push($(tr).find('td').eq(6).text().trim()); // mls_affiliate
       }
+
+      retArr.push(website); // url
+      retArr.push(`https://en.wikipedia.org${teamLink}`); // wikipedia_url
       return retArr;
     } else {
 
@@ -76,13 +104,12 @@ const results = $('.wikitable').eq(1).find('tr').get().map(async function(tr) {
     }
   } else {
     // title or something
-    console.log('HERE');
     return [];
   }
 });
 const r = await Promise.all(results);
 
-let retString = 'team,city,state,latitude,longitude,stadium,stadium_capacity,year_founded,year_joined,head_coach,mls_affiliate';
+let retString = 'team,city,state,latitude,longitude,stadium,stadium_capacity,year_founded,year_joined,head_coach,mls_affiliate,url,wikipedia_url';
 r.forEach((arr) => {
   if(arr.length > 0) {
     retString = `${retString}\n${arr.join(',')}`;
